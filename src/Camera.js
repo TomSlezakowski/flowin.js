75d429757440456f463c0cd2651f1dfc2b33dc04
Tom Slezakowski <tom@leftfieldlabs.com>
2020-02-12 23:24:23 -0800

    Beginning webcam capture methods.

--- src/Camera.js
+++ src/Camera.js
@@ -1,5 +1,118 @@
-function Camera() {
 
+class Camera {
+  constructor(props) {
+    this.params = Object.assign({
+      quality: 4,
+      element: false,
+      facing: 'user',
+      callback: false,
+    }, props);
+
+    this.state = {
+      capturing: false,
+      currentFlow: null
+    };
+
+    this.videoSource = null;
+
+    this._onCameraConnected = (stream) {
+      const bind = this._onFlowReceived.bind(this);
+      this.state.capturing = true;
+
+      if ("srcObject" in this.videoElement) {
+        this.videoElement.srcObject = stream;
+      } else {
+        this.videoElement.src = window.URL.createObjectURL(stream);
+      }
+      if (stream) {
+        this.videoElement.play();
+        this.state.currentFlow.start();
+        this.state.currentFlow.onCalculated()
+      }
+    }
+
+    this._onFlowReceived = (flow) => {
+      if (callback) {
+        callback(flow);
+      }
+    }
+
+    this._startCapture = () => {
+      const { currentFlow } = this.state;
+
+      if (currentFlow === null) {
+        const elem = this.params.element || window.document.createElement('video');
+        elem.setAttribute('autoplay', true);
+        this.videoElement = elem;
+        const flow = new VideoFlowin(elem, this.params.quality);
+        this.state.currentFlow = flow;
+      }
+
+      if (window.MediaStreamTrack.getSources) {
+        window.MediaStreamTrack.getSources((sources) => {
+          for (let i = 0; i < sources.length; i++) {
+            const { facing, kind } = sources[i];
+            if (kind === 'video'){
+              this.videoSource = sourceInfos[i].id;
+              if (facing === this.props.facing) {
+                  break;
+              }
+            }
+          }
+
+          const mediaProps = {
+            video: {
+              optional: [{
+                sourceId: this.videoSource,
+              }]
+            },
+          };
+          navigator.mediaDevices.getUserMedia(mediaProps)
+            .then(this._onCameraConnected.bind(this))
+            .catch(onWebCamFail);
+        });
+      } else if(navigator.mediaDevices.enumerateDevices) {
+        navigator.mediaDevices.enumerateDevices().then((sources) => {
+          for (let i = 0; i < sources.length; i++) {
+            const { kind, deviceId } = sources[i];
+            if(kind == "videoinput"){
+              this.videoSource = deviceId;
+            }
+          }
+
+          const mediaProps = {
+            video: {
+              optional: [{
+                sourceId: this.videoSource,
+              }]
+            },
+          };
+          navigator.mediaDevices.getUserMedia(mediaProps)
+            .then(this._onCameraConnected.bind(this))
+            .catch(onWebCamFail);
+        });
+      }
+    }
+
+    this._stopCapture = () => {
+
+    }
+  }
+
+  start() {
+    const { capturing } = this.state;
+    if (!capturing) {
+      this._startCapture();
+    }
+  }
+
+  stop() {
+    const { capturing } = this.state;
+    if (capturing) {
+      this.state.capturing = false;
+      this._stopCapture();
+    }
+  }
 }
 
-module.exports = Camera;
+export default Camera;
---
+++ src/Zone.js
@@ -0,0 +1,8 @@
+function Zone(x, y, u, v) {
+  this.x = x;
+  this.y = y;
+  this.u = u;
+  this.v = v;
+}
+
+module.exports = Zone;
