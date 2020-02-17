fbff717f2430c6654ad365eb558f33902325698b
Tom Slezakowski <tom@leftfieldlabs.com>
2020-02-17 11:07:17 -0800

    Updates builds + documentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,36 @@
 // Webpack uses this to work with directories
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist/flowin/'),
-    filename: 'index.js',
-    library: 'Flowin',
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = env => {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist/flowin/'),
+      filename: 'index.js',
+      library: 'Flowin',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+    },
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
         }
-      }
-    ]
-  },
-  watch: true,
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000,
-    ignored: ['node_modules/**', 'examples/**']
+      ]
+    },
+    watch: env === "development" ? true : false,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+      ignored: ['node_modules/**', 'examples/**']
+    }
   }
 };
